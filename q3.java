6c5ab09e6c6a88b5a3988d74f8b580df0d6e7fd1
charliep19 <charliepenny33@outlook.com>
2018-03-22 14:05:54 -0400

    first

---
+++ q3.java
@@ -0,0 +1,13 @@
+
+public class q3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String[] names= {"CSC231","CSC232","Web Design","ENG296","ECO231","MGT333","CSC238","CSC330","CSE138","MTH231"};
+		 
+		for(int i=0; i<10; i++) {
+			System.out.println(names[i]);
+	}
+
+}
+}
---
+++ q4.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class q4 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+Scanner scan=new Scanner(System.in);
+int[] numlist=new int[5];
+
+
+
+for( int i=0; i<numlist.length;i++) {
+
+numlist[i]=scan.nextInt();
+
+}System.out.println(numlist[4]);
+	}
+
+}
